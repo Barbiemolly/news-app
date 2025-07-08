02be6bf04fdacab32070e7091b48ab84a70bc3e6
Molemo Khethwa <123152642+Barbiemolly@users.noreply.github.com>
2025-07-08 16:40:11 +0200

    Update settings.py
    mariadb too long due to volume for docker, hence i changed to sqlite when using docker

--- news_portal/news_portal/settings.py
+++ news_portal/news_portal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import socket
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,19 +103,30 @@ WSGI_APPLICATION = "news_portal.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "HOST": os.environ.get("DB_HOST", "localhost"),
-        "NAME": os.environ.get("DB_NAME", "news_db"),
-        "USER": os.environ.get("DB_USER", "root"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "mole@2018"),
-        "PORT": "3306",  # Default MySQL/MariaDB port
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+# Detect if running in Docker
+IN_DOCKER = os.environ.get("IN_DOCKER") == "1"
+
+if IN_DOCKER:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.environ.get("DB_NAME", "news_db"),
+            "USER": os.environ.get("DB_USER", "root"),
+            "PASSWORD": os.environ.get("DB_PASSWORD", "mole@2018"),
+            "HOST": os.environ.get("DB_HOST", "localhost"),
+            "PORT": os.environ.get("DB_PORT", "3306"),
+            "OPTIONS": {
+                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+        }
+    }
 
 # Email setup
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
